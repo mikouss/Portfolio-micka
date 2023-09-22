3748b4e4361f47a6b6d8b2d8b8a976efc731732b
mikouss <mikou19000@outlook.fr>
2023-09-22 14:34:46 +0200

    portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 
-site: 'https://github.com/mikouss/Portfolio.github.io',
-
+site: 'https://github.com/mikouss/portfolio.git',
+base:'/portfolio',
 
 });
-
